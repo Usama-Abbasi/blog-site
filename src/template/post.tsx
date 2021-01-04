3d5a7f5db6a046394068617bb42a536367a18034
usama999abbasi <60782977+usama999abbasi@users.noreply.github.com>
2021-01-04 22:28:13 +0500

    updated

--- src/template/post.tsx
+++ src/template/post.tsx
@@ -1,16 +1,42 @@
 import React from 'react'
 import Layout from '..//component/layout';
 import Typing from 'react-typing-animation';
-import {documentToReactComponents} from '@contentful/rich-text-react-renderer'
-export default function Post({pageContext}) {
-    const {itemDetails}=pageContext;
+import firebase from "gatsby-plugin-firebase"
+import { useSelector } from "react-redux"
+import { store, setLoggedIn } from "../redux/store"
+import { documentToReactComponents } from '@contentful/rich-text-react-renderer'
+export default function Post({ pageContext }) {
+    const loggedIn = useSelector(state => state.login)
+    const Login = async () => {
+        var provider = new firebase.auth.GoogleAuthProvider()
+        await firebase
+            .auth()
+            .signInWithPopup(provider)
+            .then(function (result) { })
+            .catch(function (error) {
+                var errorMessage = error.message
+                return errorMessage
+            })
+        store.dispatch(setLoggedIn(true))
+    }
+
+    const { itemDetails } = pageContext;
     console.log(itemDetails);
     return (
         <Layout>
             <h1>{itemDetails.title}</h1>
             <h4>{itemDetails.publisheddate}</h4>
-            <img src={itemDetails.image.fluid.src}/>
-            <p style={{fontFamily:'sans-serif',fontSize:'20px'}}><Typing>{documentToReactComponents(itemDetails.body.json)}</Typing></p>
+            <img src={itemDetails.image.fluid.src} />
+            {loggedIn ?
+                <>
+                    <p style={{ fontFamily: 'sans-serif', fontSize: '20px' }}><Typing speed={50}>{documentToReactComponents(itemDetails.body.json)}</Typing></p>
+                </>
+                : <>
+                    <h3 style={{ color: "red" }}>
+                        First you need to login to read this article
+                  </h3>
+                </>}
+
         </Layout>
     )
 }
